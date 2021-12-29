1693d9e9d4de4b2115ef3f587e18a0fd6a7b40c0
hunterMotko <hunterMotko@gmail.com>
2021-12-29 17:40:03 -0500

    market lists

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,5 +1,6 @@
 class StocksController < ApplicationController
-
+  before_action :set_client
+  
   def search
     if params[:stock].present?
       @stock = Stock.new_lookup(params[:stock].upcase)
@@ -21,4 +22,18 @@ class StocksController < ApplicationController
     end
   end
 
+  def index
+    @most_active = @client.stock_market_list(:mostactive)
+    @gainers = @client.stock_market_list(:gainers)
+    @losers = @client.stock_market_list(:losers)
+    @iex_volume = @client.stock_market_list(:iexvolume)
+    @iex_percent = @client.stock_market_list(:iexpercent)
+  end
+
+  private
+
+  def set_client
+    @client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
+      endpoint: 'https://sandbox.iexapis.com/stable')
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   resources :user_stocks, only: [:create, :destroy]
+  resources :stocks, only: [:index]
   devise_for :users
   root 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stock', to: 'stocks#search'
-
 end
